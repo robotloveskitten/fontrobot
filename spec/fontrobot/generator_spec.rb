78f4c4b0123a1b57e5d239d3eb953679b3958a5e
Tim Barkow <tbarkow@goldstar.com>
2013-04-12 07:44:25 -0700

    fixing specs

--- spec/fontrobot/generator_spec.rb
+++ spec/fontrobot/generator_spec.rb
@@ -5,30 +5,29 @@ describe Fontrobot::Generator do
   let(:output_dir) { 'tmp' }
 
   context 'normally' do
-    before(:all) { Fontrobot::Generator.start([input_dir, '-o', output_dir]) }
     after(:all) { cleanup(output_dir) }
 
     it 'should create webfonts' do
+      Fontrobot::Generator.start([input_dir, '-o', output_dir]) 
       exts = %w( .woff .eot .ttf .svg )
       files = Dir[output_dir + '/*']
       files.map! { |file| File.extname(file) }
-
       exts.each { |ext| files.should include(ext) }
     end
 
     it 'should print font-face declarations in fontrobot.css' do
+      Fontrobot::Generator.start([input_dir, '-o', output_dir]) 
       stylesheet = File.read(output_dir + '/fontrobot.css')
       files = Dir[output_dir + '/*.{woff,eot,ttf,svg}']
-
       files.each do |file|
         stylesheet.should include(File.basename(file))
       end
     end
 
     it 'should print icon-* CSS classes in fontrobot.css' do
+      Fontrobot::Generator.start([input_dir, '-o', output_dir]) 
       stylesheet = File.read(output_dir + '/fontrobot.css')
       icon_names = Dir[input_dir + '/*'].map { |file| File.basename(file)[0..-5].gsub(/\W/, '-').downcase }
-
       icon_names.each do |name|
         stylesheet.should include('.icon-' + name)
       end
@@ -36,6 +35,7 @@ describe Fontrobot::Generator do
   end
 
   context 'when input_dir does not exist' do
+    after(:all) { cleanup(output_dir) }
     let(:fake_input_dir) { 'does/not/exist' }
 
     it 'should raise an error' do
@@ -45,6 +45,7 @@ describe Fontrobot::Generator do
   end
 
   context 'when input_dir does not contain vectors' do
+    after(:all) { cleanup(output_dir) }
     let(:empty_input_dir) { 'spec/fixtures/empty' }
 
     it 'should raise an error' do
@@ -54,32 +55,30 @@ describe Fontrobot::Generator do
   end
 
   context 'when flags are passed' do
+    after(:all) { cleanup(output_dir) }
+
     it 'should save output files with a custom name' do
       Fontrobot::Generator.start([input_dir, '-o', output_dir, '-n', 'customname'])
       file = Dir[File.join(output_dir, 'customname-*.ttf')].first
       File.exists?(file).should be_true
-      cleanup(output_dir)
     end
 
     it 'should exclude the filename hash' do
       Fontrobot::Generator.start([input_dir, '-o', output_dir, '--nohash'])
       file = File.join(output_dir, 'fontrobot.ttf')
       File.exists?(file).should be_true
-      cleanup(output_dir)
     end
 
     it 'should output an html test file' do
       Fontrobot::Generator.start([input_dir, '-o', output_dir, '--html'])
       file = File.join(output_dir, 'test.html')
       File.exists?(file).should be_true
-      cleanup(output_dir)
     end
 
     it 'should output .scss files' do
       Fontrobot::Generator.start([input_dir, '-o', output_dir, '--scss'])
       file = File.join(output_dir, 'fontrobot.scss')
       File.exists?(file).should be_true
-      cleanup(output_dir)
     end
 
     it 'should reorder the font sources' do
